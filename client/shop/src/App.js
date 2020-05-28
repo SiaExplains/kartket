58f46e0fec7a38ffc85397939e50b398ca34756f
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-05-28 16:41:21 +0200

    install material-icons

--- client/shop/src/App.js
+++ client/shop/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 import './App.css';
 
-function App() {
-    return (
-        <div className='App'>
-            <h3>Kartket</h3>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h3>Kartket</h3>
+                <Button color='primary'>Test Material</Button>
+            </React.Fragment>
+        );
+    }
 }
-
 export default App;
