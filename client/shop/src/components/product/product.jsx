85db732742c1ade681086f78437e0ce5c7a5e0a3
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-05-30 11:10:00 +0200

    add fonts, install sass, use theme / product item

--- client/shop/src/components/product/product.jsx
+++ client/shop/src/components/product/product.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
+import styles from './product.module.scss';
 
 class Product extends Component {
     render() {
         return (
-            <Paper elevation={3}>
-                <img alt='alternative text' src={this.props.img} />
+            <Paper elevation={3} className={styles.product}>
+                <div>
+                    <img alt='alternative text' src={this.props.img} />
+                </div>
+                <div>
+                    <h3>{this.props.title}</h3>
+                </div>
+                <div>
+                    <p>
+                        The Tessier-Ashpool ice shattered, peeling away from the
+                        Chinese program’s thrust, a worrying impression of solid
+                        fluidity, as though the shards of a broken mirror bent
+                        and elongated as they rotated.
+                    </p>
+                </div>
             </Paper>
         );
     }
