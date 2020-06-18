baa2e5a4cc359b2e820048af32105d331c85ffdb
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-06-18 11:34:07 +0200

    reaplce sime files

--- front-end/shop/src/models/product.ts
+++
@@ -1,9 +0,0 @@
-export default class ProductModel {
-    img!: string;
-    title!: string;
-
-    constructor(img: string, title: string) {
-        this.img = img;
-        this.title = title;
-    }
-}
