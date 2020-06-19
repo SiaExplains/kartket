080a422718900ec930d2481e8b58f940f9695b12
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-06-19 09:22:57 +0200

    config sass for react-typescript

--- client/shop/src/components/App.tsx
+++ client/shop/src/components/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+
 import Header from './layout/header/header';
 import Sidebar from './layout/sidebar/sidebar';
 import Gallery from './gallery/gallery';
+import classes from './App.module.scss';
 
 function App() {
     return (
         <div>
             <Header />
-            <div style={{ display: 'flex' }}>
+            <div className={classes.main}>
                 <Sidebar />
                 <Gallery />
             </div>
--- client/shop/src/components/layout/sidebar/sidebar.tsx
+++ client/shop/src/components/layout/sidebar/sidebar.tsx
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import styles from './sidebar.module.css';
 import Categories from './categories/categories';
 
-class Sidebar extends Component {
+interface Props {
+    className?: string;
+    style?: React.CSSProperties;
+    children?: React.ReactNode;
+}
+interface State {
+    isLoading: boolean;
+}
+
+class Sidebar extends Component<Props, State> {
     render() {
         return (
             <div className={styles.sidebar}>
