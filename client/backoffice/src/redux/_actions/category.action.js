8af0b6f7cca2890313b50c1f81c5bacef08fb611
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-06-28 12:53:25 +0200

    delete old files based on js

--- client/backoffice/src/redux/_actions/category.action.js
+++ client/backoffice/src/redux/_actions/category.action.js
@@ -1,3 +1,10 @@
 export const CATEGORY_ADD = 'CATEGORY_ADD';
 export const CATEGORY_EDIT = 'CATEGORY_EDIT';
 export const CATEGORY_DELETE = 'CATEGORY_DELETE';
+
+const categoryAdd = (category) => {
+    return {
+        type: CATEGORY_ADD,
+        category,
+    };
+};
