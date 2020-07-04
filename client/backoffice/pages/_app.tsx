c196dae4e8abaac9530bb392fe932b59dc9eae4b
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-07-04 11:01:37 +0200

    convert nestjs based on js to typescript

--- client/backoffice/pages/_app.tsx
+++ client/backoffice/pages/_app.tsx
@@ -1,8 +1,6 @@
-import { AppProps } from 'next/app';
 import '../styles/global.css';
+import { AppProps } from 'next/app';
 
-function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
-
-export default App;
--- client/backoffice/pages/index.tsx
+++ client/backoffice/pages/index.tsx
@@ -4,15 +4,30 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import { GetStaticProps } from 'next';
 
-export default function Home({ allPostsData }) {
+export default function Home({
+    allPostsData,
+}: {
+    allPostsData: {
+        date: string;
+        title: string;
+        id: string;
+    }[];
+}) {
     return (
         <Layout home>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>Backoffice by NextJs</p>
+                <p>[Your Self Introduction]</p>
+                <p>
+                    (This is a sample website - you’ll be building a site like
+                    this in{' '}
+                    <a href='https://nextjs.org/learn'>our Next.js tutorial</a>
+                    .)
+                </p>
             </section>
             <section
                 className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
@@ -36,11 +51,11 @@ export default function Home({ allPostsData }) {
     );
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
     const allPostsData = getSortedPostsData();
     return {
         props: {
             allPostsData,
         },
     };
-}
+};
