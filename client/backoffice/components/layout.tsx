3a63ac6202341a23ed5559903b9117dd1b4bcc28
sia.qnbr@gmail.com <sia.qnbr@gmail.com>
2020-07-08 11:38:08 +0200

    some modification for understanding nextjs better.

--- client/backoffice/components/layout.tsx
+++ client/backoffice/components/layout.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = '[Your Name]';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+
+const name = 'Kartket Backoffice';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({
